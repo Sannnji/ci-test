59ab65639a80f4e2ea72153916be0487c911bc19
James Ji <57771801+Sannnji@users.noreply.github.com>
2022-08-02 14:24:23 -0400

    changed code to fail linter and jest tests

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,6 @@ const { it } = require('mocha');
 const didShowMessage = require('../index');
 
 it('check if true', () => {
-  const check = expect(didShowMessage).to.be.false;
+  const check = expect(didShowMessage).to.be.true;
   return check;
 });
